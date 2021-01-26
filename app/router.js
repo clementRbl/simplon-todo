b1856db13c706e3a2b221b86d26cef412741b048
clementRbl <clement.rbl@gmail.com>
2021-01-26 12:30:41 +0100

    mocodo ok, wireframe ok, serveur ok

---
+++ app/router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('hello todo');
+});
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const router = require('./app/router');
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(router);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT} ...`);
+});
